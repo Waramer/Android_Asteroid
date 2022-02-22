70ec09a606fb7ec71891fea2b2586a65215f103e
Quentin Delezenne <quentin.delezenne@orange.fr>
2022-02-22 10:17:26 +0100

    First Commit : Main menu

---
+++ app/src/main/java/com/amse/asteroid/MainGame.java
@@ -0,0 +1,15 @@
+package com.amse.asteroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
